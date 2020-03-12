9ee5e0c9c6b8971e9023bf0b820108daee003f51
Philippe Rateau <philippe.rateau@imt-atlantique.net>
2020-03-12 14:56:04 +0100

    prise de connaissance avec node.js et installation de socket.io

---
+++ src/hello_world.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function(req, res) {
+    var page = url.parse(req.url).pathname;
+    console.log(page);
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    if (page == '/') {
+        res.write('Vous êtes à l\'accueil, que puis-je pour vous ?');
+    }
+    else if (page == '/sous-sol') {
+        res.write('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
+    }
+    else if (page == '/etage/1/chambre') {
+        res.write('Hé ho, c\'est privé ici !');
+    }
+    res.end();
+});
+
+var EventEmitter = require('events').EventEmitter;
+
+var jeu = new EventEmitter();
+
+jeu.on('gameover', function(message){
+    console.log(message);
+});
+
+jeu.emit('gameover', 'Vous avez perdu !');
+
+server.listen(8080);
